ddc3710175076028ac515fe38bb1578e8a267f5f
manasjhamj <jhamanas939@gmail.com>
2024-02-05 19:53:10 +0530

    Add more component

--- src/landing/landing.js
+++ src/landing/landing.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './landing.css'
 import Home from './home/home';
+import More from './more/more';
 
 function landing() {
     return (
         <>
             <Home />
+            <More />
         </>
     )
 }
---
+++ src/landing/more/more.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './more.css'
+
+function more() {
+    return (
+        <section id="des-dev" class="reveal">
+            <div class="des-dev-text">
+                <h2>Creative Design for High Quality Development.</h2>
+                <p>
+                    I oversee the entire product development journey, from crafting the interface to delivering a fully
+                    functional application. I bring ideas to life, ensuring top-notch design and leveraging cutting-edge
+                    software for a seamless user experience.
+                </p>
+            </div>
+            <div class="container">
+                <div class="container-col">
+                    <img alt="1" src="https://www.manasjha.me/images/1.svg" />
+                    <h3>Designing</h3>
+                    <p>
+                        I design beautiful user interfaces for applications to work on all screen sizes. Your colors,
+                        typography and more, all pixel perfect and beautiful.
+                    </p>
+                </div>
+                <div class="container-col">
+                    <img alt="2" src="https://www.manasjha.me/images/2.svg" />
+                    <h3>Development</h3>
+                    <p>
+                        I create functional, beautiful and responsive frontend applications for the web with modern
+                        technologies and best practices.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default more;
