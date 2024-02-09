250c201b3ded08771637bc6d94f83ca00b61bc0d
manasjhamj <jhamanas939@gmail.com>
2024-02-09 22:43:58 +0530

    add footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react'
 import Navbar from './navbar/navbar';
 import Landing from './landing/Landing';
+import Footer from './footer/footer';
 
 function App() {
   return (
     <>
       <Navbar />
       <Landing />
+      <Footer />
     </>
   );
 }
