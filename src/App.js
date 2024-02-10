ef8ada590f10b44c75735d17862bbe0fba026a63
manasjhamj <jhamanas939@gmail.com>
2024-02-10 23:06:46 +0530

    add experience component to app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react'
 import Navbar from './navbar/navbar';
 import Landing from './landing/Landing';
 import Footer from './footer/footer';
+import Experience from './experience/experience';
 
 function App() {
   return (
     <>
       <Navbar />
       <Landing />
+      <Experience />
       <Footer />
     </>
   );
