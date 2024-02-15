ca3c6e42fac1e745ecf2f64c8fea5c90bb29598b
manasjhamj <jhamanas939@gmail.com>
2024-02-15 15:00:04 +0530

    add state to connect json

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Navbar from './navbar/navbar';
 import Landing from './landing/Landing';
 import Footer from './footer/footer';
 import Experience from './experience/experience';
 
 function App() {
+  const [jsonData, setJsonData] = useState([]);
+
+  useEffect(() => {
+    fetch('/experience.json')
+      .then((response) => response.json())
+      .then((data) => setJsonData(data))
+      .catch((error) => console.error('Error fetching data: ', error));
+  });
+
   return (
     <>
       <Navbar />
       <Landing />
-      <Experience />
+      <Experience jsondata={jsonData} />
       <Footer />
     </>
   );
